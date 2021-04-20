ae6e385d60c0a06fd40547f51444ab27b68a8040
vesa193 <veselinovic93@hotmail.com>
2021-04-20 09:26:55 +0200

    - initialize commit

---
+++ src/store/common/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  anyProp: null,
+};
+
+function commonReducer(state = initialState, action) {
+  const { type } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+export default commonReducer;
